4514bf43bba0e8069aa0153b1116e72dc4dd30ae
张志君 <mail.zhijun.zhang@icloud.com>
2022-07-24 21:51:56 +0800

    更新：通过消息同步运单状态

--- src/main/java/com/sl/ms/oms/mq/MQListener.java
+++ src/main/java/com/sl/ms/oms/mq/MQListener.java
@@ -4,6 +4,7 @@ import cn.hutool.json.JSONUtil;
 import com.sl.ms.oms.dto.OrderPickupDTO;
 import com.sl.ms.oms.service.CrudOrderService;
 import com.sl.transport.common.constant.Constants;
+import com.sl.transport.common.vo.TransportOrderStatusMsg;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.core.ExchangeTypes;
 import org.springframework.amqp.rabbit.annotation.Exchange;
@@ -41,5 +42,22 @@ public class MQListener {
         log.info("接收到快递员取件成功的消息 ({})-> {}", Constants.MQ.Queues.OMS_COURIER_PICKUP_UPDATE_ORDER, msg);
         OrderPickupDTO orderPickupDTO = JSONUtil.toBean(msg, OrderPickupDTO.class);
         this.crudOrderService.orderPickup(orderPickupDTO);
+    }
+
+    /**
+     * 更新运单状态
+     *
+     * @param msg 消息
+     */
+    @RabbitListener(bindings = @QueueBinding(
+            value = @Queue(name = Constants.MQ.Queues.OMS_TRANSPORT_ORDER_UPDATE_STATUS),
+            exchange = @Exchange(name = Constants.MQ.Exchanges.TRANSPORT_ORDER, type = ExchangeTypes.TOPIC),
+            key = Constants.MQ.RoutingKeys.TRANSPORT_ORDER_UPDATE_STATUS_PREFIX + "#"
+    ))
+    public void listenTransportOrderUpdateStatusMsg(String msg) {
+        log.info("接收到更新运单状态的消息 ({})-> {}", Constants.MQ.Queues.OMS_TRANSPORT_ORDER_UPDATE_STATUS, msg);
+        TransportOrderStatusMsg transportOrderStatusMsg = JSONUtil.toBean(msg, TransportOrderStatusMsg.class);
+        //TODO 具体业务逻辑的处理
+
     }
 }
