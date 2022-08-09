40be4343fbdfa45ee4480daf0b02b05ea6ab5b26
wangxiaobing <wangxiaobing@itcast.cn>
2022-08-09 20:29:51 +0800

    订单查询

--- src/main/java/com/sl/ms/oms/service/impl/CrudOrderServiceImpl.java
+++ src/main/java/com/sl/ms/oms/service/impl/CrudOrderServiceImpl.java
@@ -126,7 +126,7 @@ public class CrudOrderServiceImpl extends ServiceImpl<OrderMapper, OrderEntity> 
         // 客户端不展示删除状态
         lambdaQueryWrapper.ne(ObjectUtil.isNotEmpty(order.getMemberId()), OrderEntity::getStatus, OrderStatus.DEL.getCode());
 
-        // 客户端根据用户ID查询
+        // 客户端根据用户ID查询 或者用收件人手机号查询都可以
         lambdaQueryWrapper.or()
                 .eq(ObjectUtil.isNotEmpty(order.getMemberId()), OrderEntity::getMemberId, order.getMemberId());
         lambdaQueryWrapper.orderBy(true, false, OrderEntity::getCreateTime);
